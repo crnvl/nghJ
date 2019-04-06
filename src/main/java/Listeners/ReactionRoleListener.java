e617f096b0294da3faa6646a63b957bcc9498f60
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2019-04-06 20:45:53 +0200

    Update ReactionRoleListener.java

--- src/main/java/Listeners/ReactionRoleListener.java
+++ src/main/java/Listeners/ReactionRoleListener.java
@@ -1,6 +1,7 @@
 package Listeners;
 
 import net.dv8tion.jda.core.events.message.guild.react.GuildMessageReactionAddEvent;
+import net.dv8tion.jda.core.events.message.guild.react.GuildMessageReactionRemoveEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class ReactionRoleListener extends ListenerAdapter {
@@ -8,12 +9,24 @@ public class ReactionRoleListener extends ListenerAdapter {
     @Override
     public void onGuildMessageReactionAdd(GuildMessageReactionAddEvent event) {
         if(event.getMessageId().contains("563780913138368532")) {
-            String reactionId = event.getReactionEmote().getName();
-            switch (reactionId) {
-                case "<:weary:563601941423325195>":
-                    event.getGuild().getController().addRolesToMember(event.getMember(), event.getGuild().getRoleById("563780537878315018")).queue();
-                    break;
-            }
-
+                String reactionId = event.getReactionEmote().getName();
+                switch (reactionId) {
+                    case "weary":
+                        event.getGuild().getController().addRolesToMember(event.getMember(), event.getGuild().getRoleById("563780537878315018")).queue();
+                        break;
+                }
         }
     }
+
+    @Override
+    public void onGuildMessageReactionRemove(GuildMessageReactionRemoveEvent event) {
+        if(event.getMessageId().contains("563780913138368532")) {
+            String reactionId = event.getReactionEmote().getName();
+            switch (reactionId) {
+                case "weary":
+                    event.getGuild().getController().removeRolesFromMember(event.getMember(), event.getGuild().getRoleById("563780537878315018")).queue();
+                    break;
+            }
+        }
+    }
+}
