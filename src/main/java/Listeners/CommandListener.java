8c861111857edb1fd2946c8f97cb7b15de6c1cc9
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2019-04-21 13:38:54 +0200

    removed nsfw check

--- src/main/java/Listeners/CommandListener.java
+++ src/main/java/Listeners/CommandListener.java
@@ -5,6 +5,7 @@ import Utility.Config;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class CommandListener extends ListenerAdapter {
@@ -13,11 +14,14 @@ public class CommandListener extends ListenerAdapter {
 
         if (event.getMessage().getContentRaw().startsWith(Config.PREFIX)
                 && !Objects.equals(event.getMessage().getAuthor().getId(), event.getJDA().getSelfUser().getId())
-                && !event.getMessage().getAuthor().isBot()
-                && event.getMessage().getTextChannel().isNSFW())
+                && !event.getMessage().getAuthor().isBot())
         {
 
-            CommandHandler.handleCommand(CommandHandler.parse.parser(event.getMessage().getContentRaw(), event, Config.PREFIX));
+            try {
+                CommandHandler.handleCommand(CommandHandler.parse.parser(event.getMessage().getContentRaw(), event, Config.PREFIX));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
         } else {
             return;
