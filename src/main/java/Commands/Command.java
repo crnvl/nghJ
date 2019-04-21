5d1c05543a94c8fd3739330c280fef34ffb82aee
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2019-04-21 13:47:42 +0200

    Update Command.java

--- src/main/java/Commands/Command.java
+++ src/main/java/Commands/Command.java
@@ -2,11 +2,9 @@ package Commands;
 
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
-import java.io.IOException;
-
 public interface Command {
 
     boolean called(String[] args, MessageReceivedEvent event);
-    void action(String[] args, MessageReceivedEvent event) throws IOException;
+    void action(String[] args, MessageReceivedEvent event);
     void executed(boolean success, MessageReceivedEvent event);
 }
